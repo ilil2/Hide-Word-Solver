5c60147b9195a864a9bb502482344dca690d9a2f
narth <nathan.hirth538@gmail.com>
2024-09-10 18:01:51 +0200

    Adding xtoRGB and xtoi function to Lib folder

---
+++ HideWordSolver/Lib/xtoRGB.c
@@ -0,0 +1,36 @@
+#include "lib.h"
+#include "err.h"
+
+void xtoRGB(const char hexa[6], int* rgb[3])
+{
+	rgb[0] = 0; rgb[1] = 0; rgb[2] = 0;
+
+	for (size_t i = 0; i < 6; i += 2)
+	{
+		if(hexa[i] >= 'a' && hexa[i] <= 'f')
+		{
+			rgb[i/2] += (hexa[i] - 'a' + 10) * 16;
+		}
+		else if(hexa[i] >= '0' && hexa[i] <= '9')
+		{
+			rgb[i/2] += (hexa[i] - '0') * 16;
+		}
+		else
+		{
+			errx(12, "Invalid hexadecimal number.");
+		}
+		
+		if(hexa[i+1] >= 'a' && hexa[i+1] <= 'f')
+		{
+			rgb[i/2] += hexa[i+1] - 'a' + 10;
+		}
+		else if(hexa[i+1] >= '0' && hexa[i+1] <= '9')
+		{
+			rgb[i/2] += hexa[i+1] - '0';
+		}
+		else
+		{
+			errx(12, "Invalid hexadecimal number.");
+		}
+	}
+}
---
+++ HideWordSolver/Lib/xtoi.c
@@ -0,0 +1,29 @@
+#include "lib.c"
+#include <err.h>
+
+int xtoi(const char *hexa)
+{
+	int res = 0;
+	size_t i = 0;
+
+	while (hexa[i])
+	{
+		res *= 16;
+		if(hexa[i] >= 'a' && hexa[i] <= 'f')
+		{
+			res += hexa[i] - 'a' + 10;
+		}
+		else if(hexa[i] >= '0' && hexa[i] <= '9')
+		{
+			res += hexa[i] - '0';
+		}
+		else
+		{
+			errx(12, "Invalid hexadecimal number.");
+		}
+
+		i += 1;
+	}
+
+	return res;
+}
