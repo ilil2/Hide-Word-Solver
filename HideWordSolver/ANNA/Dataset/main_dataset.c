86c9d2933332689c5da678812c45291e30f77fbb
narth <nathan.hirth538@gmail.com>
2024-09-13 14:33:28 +0200

    Add posiblility to convert multiple bmp to csv

--- HideWordSolver/ANNA/Dataset/main_dataset.c
+++ HideWordSolver/ANNA/Dataset/main_dataset.c
@@ -1,4 +1,5 @@
 #include "../neural_network.h"
+#include "../../Lib/lib.h"
 
 int main(int argc, char **argv)
 {
@@ -7,16 +8,19 @@ int main(int argc, char **argv)
 		errx(400, "The number of argment is invalid.");
 	}
 
-    if (argc >= 4)
+    if (argc >= 5)
     {
-        if (argv[3][0] == '1')
-        {
-            
-        }
-        else
-        {
-            add_to_dataset(argv[1], argv[2]);
-        }
-        
+		// {directory/text_to_add} {csv_file} {nb_file} {start_num} 
+		char buffer[100];
+		for (int i = atoi(argv[4]); i < atoi(argv[3]); i++)
+		{
+			buffer = {0};
+			sprintf(buffer, "%s%i.jpg", argv[1], i);
+			add_to_dataset(buffer, argv[2]);
+		}
     }
+	else
+	{
+		add_to_dataset(argv[1], argv[2]);
+	}
 }
