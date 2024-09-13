07b75320d877e3a6eb15fb95d9f72a217059b9f0
narth <nathan.hirth538@gmail.com>
2024-09-13 22:22:49 +0200

    Add S to dataset

--- HideWordSolver/ANNA/Dataset/add_to_dataset.c
+++ HideWordSolver/ANNA/Dataset/add_to_dataset.c
@@ -1,34 +1,44 @@
 #include "../neural_network.h"
 #include <unistd.h>
 
-int add_to_dataset(const char *img, const char *csv)
-{   
-	char command[256];
-	snprintf(command, sizeof(command), "magick %s image.bmp", img);
-
-	system(command);
-
-	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+void add_to_dataset(const char *img, const char *csv)
+{
+	FILE *file = fopen(img, "r");
+	if (file != NULL)
 	{
-		printf("Cannot initialise SDL: %s", SDL_GetError());
-		fflush(stdout);
-		return 1;
+		fclose(file);
+
+		char command[256];
+		snprintf(command, sizeof(command), "magick %s image.bmp", img);
+
+		system(command);
+
+		if (SDL_Init(SDL_INIT_VIDEO) != 0)
+		{
+			errx(7, "Cannot initialise SDL: %s", SDL_GetError());
+		}
+		else
+		{
+			SDL_Surface *tmp = SDL_LoadBMP("image.bmp");
+
+			if (!tmp)
+			{
+				SDL_Quit();
+				printf("Cannot load BMP image: %s", SDL_GetError());
+			}
+			else
+			{
+				bmp_to_csv(tmp, csv);
+			}
+
+			bmp_to_csv(tmp, csv);
+
+			SDL_FreeSurface(tmp);
+			SDL_Quit();
+		}
 	}
-
-	SDL_Surface *tmp = SDL_LoadBMP("image.bmp");
-
-	if (!tmp)
+	else
 	{
-		SDL_Quit();
-		printf("Cannot load BMP image: %s", SDL_GetError());
-		fflush(stdout);
-		return 1;
+		printf("The file does not exist\n");
 	}
-
-	bmp_to_csv(tmp, csv);
-
-	SDL_FreeSurface(tmp);
-    SDL_Quit();
-
-	return 0;
 }
