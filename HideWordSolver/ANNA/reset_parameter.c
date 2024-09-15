9b6d755333c0f72481cf1e2098552b1a82f36c48
Nathan Hirth <nathan.hirth538@gmail.com>
2024-09-15 23:07:03 +0200

    Modification of rest parameter

--- HideWordSolver/ANNA/reset_parameter.c
+++ HideWordSolver/ANNA/reset_parameter.c
@@ -1,5 +1,6 @@
 #include "neural_network.h"
 #include <stdlib.h>
+#include <math.h>
 #include <time.h>
 
 void reset_parameter(double **w_input, // input_neuron x hidden_neuron
@@ -7,33 +8,34 @@ void reset_parameter(double **w_input, // input_neuron x hidden_neuron
 		    double *b_input, // hidden_neuron
 		    double *b_output) // output_neuron
 {
-    
     srand(time(0));
 
+    double limit_w_input = sqrt(6.0 / (input_neuron + hidden_neuron));
     for (int i = 0; i < input_neuron; i++)
     {
         for (int j = 0; j < hidden_neuron; j++)
         {
-            w_input[i][j] = ((double)rand() / RAND_MAX) * 2.0 - 1.0;
+            w_input[i][j] = ((double)rand() / RAND_MAX) * 2.0 * limit_w_input - limit_w_input;
         }
     }
 
+    double limit_w_output = sqrt(6.0 / (hidden_neuron + output_neuron));
     for (int i = 0; i < hidden_neuron; i++)
     {
         for (int j = 0; j < output_neuron; j++)
         {
-            w_output[i][j] = ((double)rand() / RAND_MAX) * 2.0 - 1.0;
+            w_output[i][j] = ((double)rand() / RAND_MAX) * 2.0 * limit_w_output - limit_w_output;
         }
     }
 
     for (int i = 0; i < hidden_neuron; i++)
     {
-        b_input[i] = ((double)rand() / RAND_MAX) * 2.0 - 1.0;
+        b_input[i] = 0.0;
     }
 
     for (int i = 0; i < output_neuron; i++)
     {
-        b_output[i] = ((double)rand() / RAND_MAX) * 2.0 - 1.0;
+        b_output[i] = 0.0;
     }
 
     save_parameter(w_input, w_output, b_input, b_output);
