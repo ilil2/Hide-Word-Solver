6630e71667c2a9f522b81039f5be03ebc453915b
ilyann gwinner <ilyann.gwinner@gmail.com>
2024-09-17 22:20:15 +0200

    passage en multi threading des matrices

--- HideWordSolver/Lib/matrix_product.c
+++ HideWordSolver/Lib/matrix_product.c
@@ -1,26 +1,60 @@
 #include <err.h>
 #include "lib.h"
+#include <threads.h>
+
+typedef struct 
+{
+	double **mat1;
+	double **mat2;
+	double **mat3;
+	size_t start_row;
+	size_t end_row;
+	size_t row;
+	size_t col;
+	size_t both;
+} MatrixProductThreadData;
 
 void matrix_product(size_t row1, size_t col1, double **mat1,
-		size_t row2, size_t col2, double **mat2, double **res)
+		size_t row2, size_t col2, double **mat2, double **res,
+		size_t thread_nbr = 1)
 {
 	if(col1 == row2)
 	{
-    	for(size_t i = 0; i < row1; i++)
+		thrd_t threads[thread_nbr];
+
+		for (size_t i = 0; i < thread_nbr; i++)
 		{
-			for(size_t j = 0; j < col2; j++)
-			{
-				double add = 0;
-				for(size_t k = 0; k < col1; k++)
-				{
-					add += mat1[i][k] * mat2[k][j];
-				}
-				res[i][j] = add;
-			}
+			size_t start_row = (i * row1) / thread_nbr;
+			size_t end_row = ((i + 1) * row1) / thread_nbr;
+
+			MatrixProductThreadData* data = {mat1, mat2, res, start_row,
+				end_row, row1, col2, col1};
+			thrd_create(&threads[i], _matrix_product, data);
+		}
+
+		for (size_t i = 0; i < thread_nbr; i++)
+		{
+			thrd_join(threads[i], NULL);
 		}
 	}
 	else
 	{
 		errx(3, "Impossible to multiply the two matrices.");
+	}
+}
+
+void _matrix_product(void *arg)
+{
+	MatrixProductThreadData* data = (MatrixProductThreadData*)arg;
+	for (size_t i = data->start_row; i < data->end_row; i++)
+	{
+		for (size_t j = 0; j < data->col; j++)
+		{
+			mat3[i][j] = 0;
+			for (size_t k = 0; k < data->both; k++)
+			{
+				mat3[i][j] += mat1[i][k] * mat2[k][j]
+			}
+		}
 	}
 }
--- HideWordSolver/Lib/matrix_product_t.c
+++ HideWordSolver/Lib/matrix_product_t.c
@@ -1,11 +1,53 @@
 #include <err.h>
 #include "lib.h"
+#include <threads.h>
+
+typedef struct
+{
+	double **mat1;
+	double **mat2;
+	double **mat3;
+	size_t start_row;
+	size_t end_row;
+	size_t row;
+	size_t col;
+	size_t both;
+	char trans_nbr;
+} MatrixProductThreadData
 
 void matrix_product_t(size_t row1, size_t col1, double **mat1,
-        size_t row2, size_t col2, double **mat2, double **res)
+        size_t row2, size_t col2, double **mat2, double **res,
+		char trans_nbr = 0, size_t thread_nbr = 1)
 {
     if(col1 == col2)
     {
+		thrd_t threads[thread_nbr];
+
+		for (size_t i = 0; i < thread_nbr; i++)
+		{
+			size_t start_row = (i * row1) / thread_nbr;
+			size_t end_row = ((i + 1) * row1) / thread_nbr;
+
+
+			MatrixProductThreadData* data;
+			if (trans_nbr)
+			{
+				data = {mat1, mat2, res, start_row,
+					end_row, row1, row2, col1};
+			}
+			else
+			{
+				data = {mat1, mat2, res, start_row,
+					end_row, col1, col2, row1};
+			}
+			
+			thrd_create(&threads[i], _matrix_product_t, data);
+		}
+
+		for (size_t i = 0; i < thread_nbr; i++)
+		{
+			thrd_join(threads[i]);
+		}
         for(size_t i = 0; i < row1; i++)
         {
             for(size_t j = 0; j < row2; j++)
@@ -23,4 +65,38 @@ void matrix_product_t(size_t row1, size_t col1, double **mat1,
     {
         errx(3, "Impossible to multiply the two matrices.");
     }
+}
+
+void _matrix_product_t(void *arg)
+{
+	MatrixProductThreadData* data = (MatrixProductThreadData*)arg;
+
+	if (trans_nbr)
+	{
+		for (size_t i = data->start_row; i < data->end_row; i++)
+		{
+			for (size_t j = 0; i < data->col; j++)
+			{
+				mat3[i][j] = 0;
+				for (size_t k = 0; k < data->both; k++)
+				{
+					mat3[i][j] += mat1[i][k] * mat2[j][k];
+				}
+			}
+		}
+	}
+	else
+	{
+		for (size_t i = data->start_row; i < data->end_row; i++)
+		{
+			for (size_t j = 0; j < data->col; j++)
+			{
+				mat3[i][j] = 0;
+				for (size_t k = 0; k < data->both; k++)
+				{
+					mar3[i][j] += mat1[j][i] * mat2[k][j];
+				}
+			}
+		}
+	}
 }
