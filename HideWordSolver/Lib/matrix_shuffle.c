3861c96a1783cc2908c513559272e98ff9a4969d
narth <nathan.hirth538@gmail.com>
2024-09-26 13:35:21 +0200

    Fix matrix shuffle

--- HideWordSolver/Lib/matrix_shuffle.c
+++ HideWordSolver/Lib/matrix_shuffle.c
@@ -2,19 +2,22 @@
 #include <stdlib.h>
 #include <time.h>
 
-void matrix_shuffle(double **array, double **array2, int rows, int cols) {
+void matrix_shuffle(double **array, double **array2, int rows1, int rows2, int cols) {
     srand(time(NULL));
 
     for (int i = cols - 1; i > 0; i--)
     {
         int j = rand() % (i + 1);
 
-        for (int k = 0; k < rows; k++)
+        for (int k = 0; k < rows1; k++)
         {
             double temp1 = array[k][i];
             array[k][i] = array[k][j];
             array[k][j] = temp1;
+        }
 
+        for (int k = 0; k < rows2; k++)
+        {
             double temp2 = array2[k][i];
             array2[k][i] = array2[k][j];
             array2[k][j] = temp2;
